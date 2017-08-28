79e95937808ef08b34184dee821ba70e178ffb43
Giulliano Paz <giulliano94@gmail.com>
2017-08-28 09:50:30 -0300

    Initial commit

---
+++ caesar_cipher.py
@@ -0,0 +1,23 @@
+"""
+    Autor: Giulliano Paz
+    Mat: 121151618
+    Curso: Ciência da Computação
+    Inst: Universidade Federal do Pampa - UNIPAMPA
+"""
+# Alfabeto de caracteres para a encripitação
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789?/=][()@$# "
+# Recebe fator de deslocamento
+print("\n    Valores POSITIVOS para cifrar e valores NEGATIVOS para decifrar")
+shift = int(input("\n ~> Fator de deslocamento: "))
+# Recebe texto a ser cifrado
+palavra = input("\n ~> Texto: ").lower()
+
+# Mostra palavra cifrada
+print("\n ~> Resultado: ", end=" ")
+# Percorre palavra a ser cifrada, deslocando cada letra pelo fator de deslocamento
+for elem in palavra:
+	try:
+		print(alphabet[(alphabet.index(elem)+shift) % len(alphabet)], end="")
+	except Exception:
+		print(elem, end="")
+print("")
